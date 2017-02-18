6449414393c3694348232add359667305cc22d9b
chupotle <tcchu97@gmail.com>
2017-02-18 13:29:11 -0500

    pressure

--- app/src/main/java/com/threebetasonematt/a420game/HostLobbyActivity.java
+++ app/src/main/java/com/threebetasonematt/a420game/HostLobbyActivity.java
@@ -1,12 +1,16 @@
 package com.threebetasonematt.a420game;
 
+import android.content.Context;
 import android.content.Intent;
-import android.hardware.SensorEvent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 import org.w3c.dom.Text;
 
@@ -15,11 +19,15 @@ public class HostLobbyActivity extends AppCompatActivity{
     String mLobbyAddress;
     TextView mAddressLabel;
     Button mButtonReady;
+    private SensorManager mSensorManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_lobby);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE),SensorManager.SENSOR_DELAY_FASTEST);
 
         Intent intent = getIntent();
         mLobbyAddress = intent.getStringExtra(constants.KEY_HOSTING_LOBBY_ADDRESS);
@@ -45,6 +53,26 @@ public class HostLobbyActivity extends AppCompatActivity{
         });
 
     }
+    private SensorEventListener mSensorListener = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // when accuracy changed, this method will be called.
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // when pressure value is changed, this method will be called.
+            float pressure_value = 0.0f;
+
+            // if you use this listener as listener of only one sensor (ex, Pressure), then you don't need to check sensor type.
+            if( Sensor.TYPE_PRESSURE == event.sensor.getType() ) {
+                pressure_value = event.values[0];
+                mAddressLabel = (TextView)findViewById(R.id.hostlobby_address);
+                mAddressLabel.setText(String.valueOf(pressure_value));
+            }
+        }
+    };
 
 
 }
