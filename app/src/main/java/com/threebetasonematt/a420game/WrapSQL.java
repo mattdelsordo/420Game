262cc7bf494673041822070d6c8312800c2a38eb
mynameisnhan <nle10147@yahoo.com>
2017-02-18 17:39:03 -0500

    Initial commit

--- app/src/main/java/com/threebetasonematt/a420game/WrapSQL.java
+++ app/src/main/java/com/threebetasonematt/a420game/WrapSQL.java
@@ -1,3 +1,23 @@
 package com.threebetasonematt.a420game;
 
 import java.sql.*;
+import android.content.Intent;
+
+protected class WrapSQL {
+
+    String username, gameid;
+    float startingAlt, endingAlt, altitudeDifference;
+    
+
+    private String insertUserDetails(UserDetails ud)
+    {
+
+    }
+
+    private class UserDetails
+    {
+
+    }
+
+
+}
