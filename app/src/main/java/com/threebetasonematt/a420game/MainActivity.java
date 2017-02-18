d3ca6a0bf59406b130cdde9cd48c40907953e8dd
mattdelsordo <mattdelsordo@gmail.com>
2017-02-17 21:41:03 -0500

    Figured out how to organize all this so itll be faster now

--- app/src/main/java/com/threebetasonematt/a420game/MainActivity.java
+++ app/src/main/java/com/threebetasonematt/a420game/MainActivity.java
@@ -6,12 +6,41 @@ Handles logging in and starting a game.
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    Button mPlayButton;
+    EditText mEnterUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get reference for username box
+        mEnterUsername = (EditText)findViewById(R.id.edittext_enter_username);
+
+        //handle play button
+        mPlayButton = (Button)findViewById(R.id.button_splash_play);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mEnterUsername.getText().toString();
+                if(username.equals("") || username.equals(null)){
+                    //print toast
+                }
+                else{
+                    //open next activity
+                }
+            }
+        });
+
+
     }
 }
