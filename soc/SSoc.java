0f64d6b8860f3247ce84207c37aca5ec7a929bf3
chupotle <tcchu97@gmail.com>
2017-02-19 07:51:07 -0500

    adsasdf

--- soc/SSoc.java
+++ soc/SSoc.java
@@ -47,7 +47,8 @@ public class SSoc{
 									ready=true;
 									//send back ready
 									for(int j=0; j<players;j++ ){
-										//pWriter[j].println("ready");
+										pWriter[j].println("ready");
+										pWriter[j].flush();
 									}
 
 								}
