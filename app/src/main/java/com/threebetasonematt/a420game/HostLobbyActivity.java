f34f136dfe37de97992ef33b283fab80fec50783
chupotle <tcchu97@gmail.com>
2017-02-19 08:00:15 -0500

    final

--- app/src/main/java/com/threebetasonematt/a420game/HostLobbyActivity.java
+++ app/src/main/java/com/threebetasonematt/a420game/HostLobbyActivity.java
@@ -77,16 +77,16 @@ public class HostLobbyActivity extends AppCompatActivity{
 
                 //calculate initial altitude
                 float initialAltitude = mSensorManager.getAltitude(mSensorManager.PRESSURE_STANDARD_ATMOSPHERE, mCurrentPressure);
-//                Socket soc=null;
-//                PrintWriter pw=null;
-//                try {
-//                    soc = new Socket("184.72.127.7", 4333);
-//                    SocketHandler.setSocket(soc);
-//                    pw = SocketHandler.getPW();
-//                    pw.println("ready");
-//                    pw.flush();
-//                }
-//                catch(Exception e){mAddressLabel.setText("butts");}
+                Socket soc=null;
+                PrintWriter pw=null;
+                try {
+                    soc = new Socket("184.72.127.7", 4333);
+                    SocketHandler.setSocket(soc);
+                    pw = SocketHandler.getPW();
+                    pw.println("ready");
+                    pw.flush();
+                }
+                catch(Exception e){mAddressLabel.setText("butts");}
 
                 //wait for start from server
 //                try{
