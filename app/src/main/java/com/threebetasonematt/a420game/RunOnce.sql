8bbf7d6dcf3779d1791c3e8e926e4a263f709cde
mynameisnhan <nle10147@yahoo.com>
2017-02-18 20:31:28 -0500

    Initial commit

--- app/src/main/java/com/threebetasonematt/a420game/RunOnce.sql
+++ app/src/main/java/com/threebetasonematt/a420game/RunOnce.sql
@@ -3,24 +3,24 @@ DROP TABLE IF EXISTS User;
 CREATE TABLE User (
     userID varchar(10) NOT NULL PRIMARY KEY,
     winCount int NOT NULL,
-    highScore decimal(10000, 2) NOT NULL -- ###.##
+    highScore decimal(6, 2) NOT NULL -- ###.##
 );
 
 DROP TABLE IF EXISTS Game;
 
 CREATE TABLE Game (
     gameID varchar(17) NOT NULL PRIMARY KEY,
-    userID varchar(10) FOREIGN KEY REFERENCES User(userID),
-    startingAltitude decimal(1000000, 2) NOT NULL,
-    endingAltitude decimal(1000000, 2) NOT NULL,
-    score decimal(10000, 2) NOT NULL,
+    userID varchar(10) REFERENCES User(userID),
+    startingAltitude decimal(8, 2) NOT NULL,
+    endingAltitude decimal(8, 2) NOT NULL,
+    score decimal(6, 2) NOT NULL,
     winStatus binary(1) NOT NULL
 );
 
 DROP TABLE IF EXISTS Leader;
 
 CREATE TABLE Leader (
-    userID varchar(10) FOREIGN KEY REFERENCES User(userID),
-    highScore decimal(10000, 2) FOREIGN KEY REFERENCES User(highScore)
+    userID varchar(10) REFERENCES User(userID),
+    highScore decimal(6, 2) REFERENCES User(highScore)
 );
 
