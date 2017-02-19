98a3820ee5aae8e8917d85e64b0867b361907e90
chupotle <tcchu97@gmail.com>
2017-02-18 23:50:01 -0500

    socket punch

--- soc/SSoc.java
+++ soc/SSoc.java
@@ -1,15 +1,35 @@
 import java.net.*;
+import java.io.*;
 
 public class SSoc{
-	
-	public static void main(String args[]){
-		System.out.println("SSoc Start");
-		try{
-			ServerSocket ss = new ServerSocket(4333);
-			ss.accept();
-			ss.close();
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+private static ServerSocket serverSocket;
+private static Socket clientSocket;
+private static BufferedReader bufferedReader;
+private static String inputLine;
+private static int port = 4333;
+
+public static void main(String[] args){
+    // Wait for client to connect on 63400
+    try{
+        System.out.println("Starting the socket server at port:" + port);
+        serverSocket = new ServerSocket(port);
+
+        System.out.println("Waiting for clients...");
+        clientSocket = serverSocket.accept();
+
+        System.out.println("You're now connected to the server.");
+
+        //START of DB connection
+        try{
+            bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+						System.out.println(bufferedReader.readLine());
+
+        } catch (Exception err){
+
+				}
+        //END of DB connection
+
+    } catch(Exception e) {
+    }
+}
 }
